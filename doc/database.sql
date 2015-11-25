5f62d1a1074d92c1ff327867d8aa0ef9e2ceeabb
kadrim1 <kadri.mihhailova@khk.ee>
2015-11-25 18:30:44 +0200

    Lisada andmebaasi testandmed [Delivers #108793610]

--- doc/database.sql
+++ doc/database.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Loomise aeg: Nov 25, 2015 kell 05:27 PL
+-- Serveri versioon: 5.6.26
+-- PHP versioon: 5.6.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -8,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bankstuff`
+-- Andmebaas: `net_bank`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction`
+-- Tabeli struktuur tabelile `transaction`
 --
 
 CREATE TABLE IF NOT EXISTS `transaction` (
@@ -24,12 +33,21 @@ CREATE TABLE IF NOT EXISTS `transaction` (
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `description` varchar(255) NOT NULL DEFAULT '""',
   `amount` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Andmete tõmmistamine tabelile `transaction`
+--
+
+INSERT INTO `transaction` (`transaction_id`, `origin_account`, `destination_account`, `date`, `description`, `amount`) VALUES
+(1, 73647364, 3837823982, '2015-11-25 12:07:57', '"Katse1"', 50),
+(2, 3837823982, 6373627236, '2015-11-25 12:07:57', '"Katse2"', 10),
+(3, 3837823982, 73647364, '2015-11-25 12:07:57', '"Katse3"', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tabeli struktuur tabelile `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -38,15 +56,24 @@ CREATE TABLE IF NOT EXISTS `users` (
   `owner_name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `password` varchar(255) CHARACTER SET utf8 NOT NULL,
   `account_number` bigint(20) NOT NULL,
-  `amount` float NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `amount` float NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Andmete tõmmistamine tabelile `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `owner_name`, `password`, `account_number`, `amount`) VALUES
+(1, 'demo', 'Demo', 'demo', 73647364, 4000),
+(2, 'epood1', 'epood', 'epood', 6373627236, 100),
+(3, 'klient1', 'klient1', 'klient1', 3837823982, 30000);
+
+--
+-- Indeksid tõmmistatud tabelitele
 --
 
 --
--- Indexes for table `transaction`
+-- Indeksid tabelile `transaction`
 --
 ALTER TABLE `transaction`
   ADD UNIQUE KEY `transaction_id` (`transaction_id`),
@@ -54,7 +81,7 @@ ALTER TABLE `transaction`
   ADD KEY `destination_account` (`destination_account`);
 
 --
--- Indexes for table `users`
+-- Indeksid tabelile `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
@@ -62,25 +89,25 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `account_number` (`account_number`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT tõmmistatud tabelitele
 --
 
 --
--- AUTO_INCREMENT for table `transaction`
+-- AUTO_INCREMENT tabelile `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `transaction_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT tabelile `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
--- Constraints for dumped tables
+-- Tõmmistatud tabelite piirangud
 --
 
 --
--- Constraints for table `transaction`
+-- Piirangud tabelile `transaction`
 --
 ALTER TABLE `transaction`
   ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`origin_account`) REFERENCES `users` (`account_number`),
